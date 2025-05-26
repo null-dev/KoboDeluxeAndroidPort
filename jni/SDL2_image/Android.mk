aa837070e8bc44b2f7d244f7b829493b63aee0cd
Andy Bao <contact@andybao.me>
2025-05-26 01:09:59 -0400

    JPEG 4 the weak

--- jni/SDL2_image/Android.mk
+++ jni/SDL2_image/Android.mk
@@ -6,7 +6,7 @@ LOCAL_MODULE := SDL2_image
 
 # Enable this if you want to support loading JPEG images
 # The library path should be a relative path to this directory.
-SUPPORT_JPG := true
+SUPPORT_JPG := false
 JPG_LIBRARY_PATH := external/jpeg-9
 
 # Enable this if you want to support loading PNG images
